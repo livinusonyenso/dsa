1c95c0711e3eeac4a1a698fbcf8470577d4db4b2
livinusonynso <livinusonyenso@gmail.com>
2025-07-27 22:24:53 +0100

    Hash Table

---
+++ Linked List/ReverseLinkedList.ts
@@ -0,0 +1,17 @@
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val: number, next: ListNode | null = null) {
+    this.val = val;
+    this.next = next;
+  }
+}
+
+
+function reverseList(head: ListNode | null): ListNode | null {
+  if (!head || !head.next) return head;
+  const newHead = reverseList(head.next);
+  head.next.next = head;
+  head.next = null;
+  return newHead;
+}
